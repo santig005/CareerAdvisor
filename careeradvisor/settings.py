d8f1ff2fd60c1d8b45e9c0fa35c5c12a03a274c6
VictorJVA <vjvilladia@eafit.edu.co>
2023-11-21 00:20:53 -0500

    embedding

--- careeradvisor/settings.py
+++ careeradvisor/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'careeradvisor',
         'USER': 'root',
-        'PASSWORD': '1234',
+        'PASSWORD': 'vic12345678',
         'HOST':'localhost',
         'PORT':'3306',
     }
